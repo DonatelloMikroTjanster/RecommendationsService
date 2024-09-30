1426b9d922c6a78aa6aff980aae3a33175abdcd3
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2024-09-30 14:50:59 +0200

    added several fields

--- src/main/java/se/salts/recommendationsservice/Entities/Media.java
+++ src/main/java/se/salts/recommendationsservice/Entities/Media.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "media")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -17,11 +19,28 @@ public class Media {
     @Column(name = "title", nullable = false)
     private String title;
 
+
+    @Column(name = "media_type", nullable = false, length = 100)
+    private String mediaCategory;
+
+    @Column(name = "genre", nullable = false, length = 100)
+    private String genre;
+
+
+    @Column(name = "release_date", nullable = false, length = 100)
+    private LocalDate releaseDate;
+
+    @Column(name = "url", nullable = false, length = 100)
+    private String url;
+
+
+    @Column(name = "duration", nullable = false, length = 100)
+    private String duration;
+
    /* @ManyToOne
     @JoinColumn(name = "genre_id")
     private Genre genre; */
-   @Column(name = "genre", nullable = false, length = 100)
-   private String genre;
+
 
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -46,6 +65,38 @@ public class Media {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getMediaCategory() {
+        return mediaCategory;
+    }
+
+    public void setMediaCategory(String mediaCategory) {
+        this.mediaCategory = mediaCategory;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
     }
 
     public String getGenre() {
